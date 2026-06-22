f1342eb39db50134db375d085d4bdec213ee0fcc
h1lsx <lsq831@proton.me>
2026-06-22 11:28:49 -0400

    a

--- src/timestamp-fix.cpp
+++ src/timestamp-fix.cpp
@@ -4,12 +4,8 @@ DWORD WINAPI func(void* me) {
     auto proc = GetCurrentProcess();
     auto base = reinterpret_cast<char*>(GetModuleHandle(NULL));
     using u8 = unsigned char;
-    static const u8 patch1[] = {0xEB, 0x19};
-    WriteProcessMemory(proc, base + 0x1518d7, patch1, 2, NULL);
-    static const u8 patch2[] = {0xEB, 0x09};
-    WriteProcessMemory(proc, base + 0x15191a, patch2, 2, NULL);
-    static const u8 patch3[] = {0xEB, 0x11};
-    WriteProcessMemory(proc, base + 0x15192b, patch3, 2, NULL);
+    static const u8 patch[] = {0x3F, 0x00, 0x00, 0x00};
+    WriteProcessMemory(proc, base + 0x20A677, patch, 4, NULL);
 
     FreeLibraryAndExitThread(reinterpret_cast<HMODULE>(me), 0);
     return 0;
